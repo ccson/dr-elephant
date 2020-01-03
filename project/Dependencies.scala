460471cf653048fcf605d5d9efbe48949f7ea549
Colin Son <colins@thinknear.com>
2020-01-02 19:16:19 -0800

    [CAGE-6323] POC

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
   lazy val jacksonVersion = "2.5.3"
   lazy val jerseyVersion = "2.24"
   lazy val jsoupVersion = "1.7.3"
-  lazy val mysqlConnectorVersion = "5.1.36"
+  lazy val mysqlConnectorVersion = "8.0.18"
   lazy val oozieClientVersion = "4.2.0"
   lazy val tonyVersion = "0.3.16"
 
